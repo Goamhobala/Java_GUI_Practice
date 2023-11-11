d6b49fcb928732cd469c40fb4e2ee5862d76fb19
Goamhobala <youyasushi@youyasushis-MacBook-Air.local>
2023-11-11 10:01:00 +0200

    inital commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class Main{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(800, 600);
+        frame.setTitle("Fuck");
+        frame.setDefaultCloseOperation(EXIT_ON_END);
+    }
+}
